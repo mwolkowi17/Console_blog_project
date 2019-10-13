930f0250c92ccbf362a33e78ead04f7029a7ea5b
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2019-10-13 15:01:52 +0200

    List

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 
 namespace Console_Blog
 {
@@ -15,8 +14,8 @@ namespace Console_Blog
                 db.SaveChanges();
                 Console.WriteLine("Console Blog Platform");
                 var blog = db.Blogs
-                    .OrderBy(b => b.BlogId)
-                    .First();
+                   .OrderBy(b => b.BlogId)
+                   .First();
                 bool control_a = true;
                 while (control_a == true)
                 {
@@ -32,35 +31,37 @@ namespace Console_Blog
                         Content = b
                     });
                     db.SaveChanges();
-                    Console.WriteLine("Jeśli chcesz wyświetlić zawartość bloga wprowadź w");
-                    Console.WriteLine("Jeśłi chcesz wprowadzić kolejny wpis wciśnij dowony inny klawisz.");
-                    string choice_a = Console.ReadLine();
-                    if (choice_a == "w")
-                    {
-                        var number = db.Posts
-                            .ToList();
-                        Console.WriteLine("Na blogu jest: "+number.Count + " wpisów");
-                        Console.WriteLine("Wprowadź liczbę wpisów które chcesz obaczyć");
-                        string choice_b = Console.ReadLine();
-                        int int_choice_b = Convert.ToInt32(choice_b);
-                        for (int n=1; n<=int_choice_b; n++)
-                        {
-                            var title = db.Posts
-                        .Single(b => b.PostId == n).Title;
-                            string title_string = Convert.ToString(title);
+                    // próba wylistowania rekordów
 
-                            var body = db.Posts
-                                .Single(b => b.PostId == n).Content;
-                            string body_string = Convert.ToString(body);
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine("=====================================================================");
-                            Console.WriteLine(title_string);
-                            Console.WriteLine(body_string);
-                            Console.WriteLine("=====================================================================");
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
+                    var lista = db.Posts.ToList();
+
+                    foreach( var rekord in lista)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("=====================================================================");
+                        Console.WriteLine(rekord.PostId);
+                        Console.WriteLine(rekord.Title);
+                        Console.WriteLine(rekord.Content);
+                        Console.WriteLine("=====================================================================");
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
                     
+                    Console.WriteLine("Jeśli chcesz wprowadzić kolejny wpis wciśnij dowony inny klawisz.");
+                    string choice_a = Console.ReadLine();
+                    //usunięcie konkretndo posta (o danym ID o ile istnieje)
+                    Console.WriteLine("Wprowadź numer post do usunięcia:");
+                    string o = Console.ReadLine();
+                    int p = Convert.ToInt32(o);
+                    //w tej chwili zawsze usuwa 2
+                    var singlePost = db.Posts
+                        .Single(b => b.PostId == p);
+
+                    Console.WriteLine(singlePost);
+                    db.Remove(singlePost);
+                    //db.Remove(blog);
+                    db.SaveChanges();
+                    
+
 
                 }
 
